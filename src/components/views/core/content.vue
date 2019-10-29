f4a59626617eb5cce30760540a8f660dab9c1054
Labib Imam <labib.imam.hidayat15@gmail.com>
2019-10-29 16:27:19 +0700

    content change

--- src/components/views/core/content.vue
+++ src/components/views/core/content.vue
@@ -1,18 +1,25 @@
 <template>
     <v-content style="height : 100%;">
         <v-container style="height : 100%;" fluid>
-            <v-row v-if="$route.name != 'welcomePage'">
-                <v-col>
-                    <h1>{{$route.meta.title}}</h1>
-                    <v-divider></v-divider>
-                </v-col>
-            </v-row>
-           <slot></slot>
+            <v-card v-if="$route.name != 'welcomePage'">
+                <v-row class="row-content">
+                    <v-col>
+                        <h1>{{$route.meta.title}}</h1>
+                        <v-divider></v-divider>
+                    </v-col>
+                </v-row>
+                <slot></slot>
+            </v-card>
+            <slot v-else></slot>
         </v-container>
     </v-content>
 </template>
 <style>
     .v-content__wrap {
         background: #d1e4e6;
+    }
+    .v-content__wrap .row-content{
+        margin-right: unset;
+        margin-left: unset;
     }
 </style>
